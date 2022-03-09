69a8339cf60b5de3402b5c47d5204c750bef422e
Rich Beilfuss <rdebeilfuss0613@gmail.com>
2022-03-09 11:27:14 -0600

    add files to repo

---
+++ app.py
@@ -0,0 +1,55 @@
+from flask import Flask, request, render_template, redirect, flash, session
+from flask_debugtoolbar import DebugToolbarExtension
+from surveys import satisfaction_survey as survey
+
+RESPONSES_KEY = "responses"
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "secret_secret"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+
+debug = DebugToolbarExtension(app)
+
+@app.route("/")
+def root_survey():
+    return render_template("start_survey.html", survey=survey)
+
+@app.route("/start", methods=["POST"])
+def start_survey():
+    session[RESPONSES_KEY] = []
+
+    return redirect("/questions/0")
+
+@app.route("/answer", methods=["POST"])
+def direct_question():
+    choice = request.form["answer"]
+
+    responses = session [RESPONSES_KEY]
+    responses.append(choice)
+    session[RESPONSES_KEY] = responses
+
+    if (len(responses) == len(survey.questions)):
+        return redirect("/finished")
+
+    else:
+        return redirect(f"/questions/{len(responses)}")
+
+@app.route("/questions/<int:qid>")
+def show_question(qid):
+    responses = session.get(RESPONSES_KEY)
+
+    if(responses is None):
+        return redirect("/")
+    if (len(responses) == len(survey.questions)):
+        return redirect("/finished")
+    if (len(responses) != qid):
+        flash(f"Invalid question id: {qid}.")
+        return redirect(f"/questions/{len(responses)}")
+
+    question = survey.questions[qid]
+    return render_template("question.html", question_num=qid, question=question)
+
+@app.route("/finished")
+def finished():
+    return render_template("finished.html")
